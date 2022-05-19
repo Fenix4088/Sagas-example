70c959810c8377af3ed8575d5195af51b688cb1b
Fenix4088 <fenix4088@gmail.com>
2022-05-19 21:34:18 +0300

    Lesson 2

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,31 +1,43 @@
-import React from 'react';
+import React, {ComponentType} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
-import { history, store } from './redux';
-import { Provider } from 'react-redux';
-import { Blog } from './pages/Blog';
-import { NotFound } from './pages/NotFound';
-import { ConnectedRouter } from "connected-react-router";
-import { Route, Routes } from "react-router";
-import { BrowserRouter } from 'react-router-dom';
+import {store} from './redux';
+import {Provider, useDispatch} from 'react-redux';
+import Blog from './pages/Blog';
+import {NotFound} from './pages/NotFound';
+import {Route, Routes} from "react-router";
+import {BrowserRouter} from 'react-router-dom';
+import {setPathname} from "./redux/reducers";
+
+
+export function withPathname<T>(WrappedComponent: ComponentType<T>): (props: any) => JSX.Element {
+    return (props: any) => {
+        const dispatch = useDispatch();
+        React.useEffect(() => {
+            dispatch(setPathname(window.location.pathname))
+        }, [dispatch])
+
+        return <WrappedComponent {...props}/>
+
+    }
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      {/*<ConnectedRouter history={history}>*/}
-        <BrowserRouter>
-        <Routes>
-          <Route path={'/'} element={<App />} />
-          <Route path={'/blog'} element={<Blog />} />
-          <Route path={'*'} element={<NotFound />} />
-        </Routes>
-        </BrowserRouter>
-      {/*</ConnectedRouter>*/}
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={'/'} element={<App/>}/>
+                    <Route path={'/blog'} element={<Blog/>}/>
+                    <Route path={'*'} element={<NotFound/>}/>
+                </Routes>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 reportWebVitals();
